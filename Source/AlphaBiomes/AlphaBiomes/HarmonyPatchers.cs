6bd33a6e155e73047403ebb7129d39b7706d7344
juanosarg <31105173+juanosarg@users.noreply.github.com>
2019-08-16 21:09:25 +0200

    v0.1

--- Source/AlphaBiomes/AlphaBiomes/HarmonyPatchers.cs
+++ Source/AlphaBiomes/AlphaBiomes/HarmonyPatchers.cs
@@ -28,6 +28,97 @@ namespace AlphaBiomes
     }
 
 
+    /*This Harmony Postfix allows us to remove gravel from a certain biome
+    */
+    [HarmonyPatch(typeof(GenStep_Terrain))]
+    [HarmonyPatch("TerrainFrom")]
+    public static class GenStep_Terrain_TerrainFrom_Patch
+    {
+        [HarmonyPostfix]
+        public static void RemoveGravel(Map map,ref TerrainDef __result)
+
+        {
+            Log.Message(__result.defName);
+            if ((__result == TerrainDefOf.Gravel)&&(map.Biome.defName== "AB_MechanoidIntrusion"))
+            {
+                Log.Message("Detectado e intentando cambiar");
+                __result = TerrainDef.Named("AB_SoilOnCrackedMetal");
+            }
+
+        }
+
+    }
+
+    /*This Harmony Prefix allows us to remove rock filth
+ */
+    [HarmonyPatch(typeof(GenStep_RockChunks))]
+    [HarmonyPatch("GrowLowRockFormationFrom")]
+    public static class GenStep_RockChunks_GrowLowRockFormationFrom_Patch
+    {
+        [HarmonyPrefix]
+        public static bool RemoveRockFilth(ref IntVec3 root, ref Map map)
+
+        {
+            if ((map.Biome.defName == "AB_MechanoidIntrusion"))
+            {
+                ThingDef mineableThing = Find.World.NaturalRockTypesIn(map.Tile).RandomElement<ThingDef>().building.mineableThing;
+                Rot4 random = Rot4.Random;
+                MapGenFloatGrid elevation = MapGenerator.Elevation;
+                IntVec3 intVec = root;
+                for (; ; )
+                {
+                    Rot4 random2 = Rot4.Random;
+                    if (!(random2 == random))
+                    {
+                        intVec += random2.FacingCell;
+                        if (!intVec.InBounds(map) || intVec.GetEdifice(map) != null || intVec.GetFirstItem(map) != null)
+                        {
+                            break;
+                        }
+                        if (elevation[intVec] > 0.55f)
+                        {
+                            return false;
+                        }
+                        if (!map.terrainGrid.TerrainAt(intVec).affordances.Contains(TerrainAffordanceDefOf.Heavy))
+                        {
+                            return false;
+                        }
+                        GenSpawn.Spawn(mineableThing, intVec, map, WipeMode.Vanish);
+                        foreach (IntVec3 b in GenAdj.AdjacentCellsAndInside)
+                        {
+                            if (Rand.Value < 0.5f)
+                            {
+                                IntVec3 c = intVec + b;
+                                if (c.InBounds(map))
+                                {
+                                    bool flag = false;
+                                    List<Thing> thingList = c.GetThingList(map);
+                                    for (int j = 0; j < thingList.Count; j++)
+                                    {
+                                        Thing thing = thingList[j];
+                                        if (thing.def.category != ThingCategory.Plant && thing.def.category != ThingCategory.Item && thing.def.category != ThingCategory.Pawn)
+                                        {
+                                            flag = true;
+                                            break;
+                                        }
+                                    }
+                                    if (!flag)
+                                    {
+                                        FilthMaker.MakeFilth(c, map, DefDatabase<ThingDef>.GetNamed("AB_SlagRubble"), 1);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+            else return true;
+
+        }
+
+    }
+
     /*This Harmony Postfix allows us to create new rock types than only appear in certain biomes
     */
     [HarmonyPatch(typeof(World))]
@@ -66,11 +157,19 @@ namespace AlphaBiomes
                
                 __result = replacedList;
             }
+            else if (__instance.grid.tiles[tile].biome.defName == "AB_MechanoidIntrusion")
+            {
+                List<ThingDef> replacedList = new List<ThingDef>();
+                ThingDef item = DefDatabase<ThingDef>.GetNamed("GU_AncientMetals");
+                replacedList.Add(item);
+
+                __result = replacedList;
+            }
             else {
                 Rand.PushState();
                 Rand.Seed = tile;
                 List<ThingDef> list = (from d in DefDatabase<ThingDef>.AllDefs
-                                       where d.category == ThingCategory.Building && d.building.isNaturalRock && !d.building.isResourceRock && !d.IsSmoothed && d.defName!= "GU_RoseQuartz" && d.defName != "AB_Mudstone" && d.defName != "AB_SlimeStone"
+                                       where d.category == ThingCategory.Building && d.building.isNaturalRock && !d.building.isResourceRock && !d.IsSmoothed && d.defName!= "GU_RoseQuartz" && d.defName != "AB_Mudstone" && d.defName != "AB_SlimeStone" && d.defName != "GU_AncientMetals"
                                        select d).ToList<ThingDef>();
                 int num = Rand.RangeInclusive(2, 3);
                 if (num > list.Count)
