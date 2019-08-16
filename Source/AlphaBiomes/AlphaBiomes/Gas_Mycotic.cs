e3dd13b65d408c789f46bdf7a8d683b5fd535e51
juanosarg <31105173+juanosarg@users.noreply.github.com>
2019-08-16 17:48:04 +0200

    v0.1

--- Source/AlphaBiomes/AlphaBiomes/Gas_Mycotic.cs
+++ Source/AlphaBiomes/AlphaBiomes/Gas_Mycotic.cs
@@ -27,7 +27,7 @@ namespace AlphaBiomes
                         {
                             Pawn pawn = current as Pawn;
                             bool flag = (pawn != null);
-                            if (flag && pawn.def.defName != "AA_Wildpod" && pawn.def.defName != "AA_Wildpawn" && pawn.def.defName != "AB_Agaripod" && pawn.def.defName != "AB_MycoidColossus")
+                            if (flag && pawn.def.defName != "AA_Wildpod" && pawn.def.defName != "AA_Wildpawn" && pawn.def.defName != "AA_Agaripod" && pawn.def.defName != "AA_MycoidColossus")
                             {
                                 pawn.TakeDamage(new DamageInfo(DamageDefOf.Cut, 1, 0f, -1f, null, null, null, DamageInfo.SourceCategory.ThingOrUnknown));
                                 HealthUtility.AdjustSeverity(pawn, HediffDefOf.ToxicBuildup, (float)0.050);
