4bf5a55f60277d04025c66a7ee4607d4b9f29bc5
juanosarg <31105173+juanosarg@users.noreply.github.com>
2019-09-07 21:22:56 +0200

    v1.1

---
+++ Source/AlphaBiomes/AlphaBiomes/BiomeWorker_PropaneLakes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using RimWorld.Planet;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.Noise;
+
+namespace AlphaBiomes
+{
+    public class BiomeWorker_PropaneLakes : BiomeWorker
+    {
+        public override float GetScore(Tile tile, int tileID)
+        {
+            if (tile.temperature > -35f)
+            {
+                return 0f;
+            }
+            return BiomeWorker_PropaneLakes.PermaIceScore(tile);
+        }
+
+        public static float PermaIceScore(Tile tile)
+        {
+            return -19f + -tile.temperature * 2f;
+        }
+    }
+}
