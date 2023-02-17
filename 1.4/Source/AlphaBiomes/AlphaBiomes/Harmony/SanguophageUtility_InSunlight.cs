c45d0cf4f89cfcd153f8d156317e3954ff84e64f
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-02-17 12:12:08 +0100

    v3.08

---
+++ 1.4/Source/AlphaBiomes/AlphaBiomes/Harmony/SanguophageUtility_InSunlight.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Collections.Generic;
+using RimWorld.Planet;
+using System.Linq;
+using System;
+using RimWorld.BaseGen;
+
+
+namespace AlphaBiomes
+{
+
+    /*Vampires are fine in the Forsaken Crags. Phytokin are fucked*/
+
+    [HarmonyPatch(typeof(SanguophageUtility))]
+    [HarmonyPatch("InSunlight")]
+    public static class AlphaBiomes_SanguophageUtility_InSunlight_Patch
+    {
+        [HarmonyPostfix]
+        public static void VampiresAreFine(ref bool __result, Map map)
+
+        {
+
+            if (map.Biome == InternalDefOf.AB_RockyCrags)
+            {
+                __result=false;
+            }
+
+        }
+
+    }
+
+
+}
